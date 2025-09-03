8af109f083f9fb5ecbc93c836e4f0185e344add8
Synthorix <jerricho1423@gmail.com>
2025-09-03 15:41:12 +0800

    Update Tzsynth.lua

--- Tzsynth.lua
+++ Tzsynth.lua
@@ -1,4 +1,4 @@
-local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/hdbfishwh/SynthorixV2/refs/heads/main/Theme.lua"))()
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/hdbfishwh/Tzsynth/refs/heads/main/Theme.lua"))()
 
 -- Services
 local Players = game:GetService("Players")
